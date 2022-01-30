b6305a3c72e02735ea73f314ccfd0824184849c7
aminslg@gmail.com <aminslg@gmail.com>
2022-01-29 23:35:51 -0500

    sql code added

---
+++ sql/test.sql
@@ -0,0 +1,4 @@
+select * from `Twitter`.testtest limit 10;
+select  * from `Twitter`.twitter_profile limit 10;
+select description from `Twitter`.twitter_profile limit 10;
+select * from mysql.engine_cost limit 10;
